c94bffa67d4ca1a0f6ff386b8ff2040fa5bc8945
Phil Stephenson <phil.stephenson@gmail.com>
2021-10-19 15:52:40 -0700

    Add OctoPetShop Azure web apps

--- main.tf
+++ main.tf
@@ -46,3 +46,63 @@ resource "octopusdeploy_azure_service_principal" "octopusdeploy_onmicrosoft_com"
   tenant_id       = var.azure_service_principal.tenant_id
   environments    = [octopusdeploy_environment.development.id, octopusdeploy_environment.staging.id, octopusdeploy_environment.production.id]
 }
+
+resource "octopusdeploy_azure_web_app_deployment_target" "ops-web-development" {
+  account_id                        = octopusdeploy_azure_service_principal.octopusdeploy_onmicrosoft_com.id
+  environments                      = [octopusdeploy_environment.development.id]
+  name                              = "OctoPetShop Web - Development"
+  resource_group_name               = "octopetshop-demo"
+  roles                             = ["web"]
+  tenanted_deployment_participation = "Untenanted"
+  web_app_name                      = "ops-web-development"
+}
+
+resource "octopusdeploy_azure_web_app_deployment_target" "ops-web-staging" {
+  account_id                        = octopusdeploy_azure_service_principal.octopusdeploy_onmicrosoft_com.id
+  environments                      = [octopusdeploy_environment.staging.id]
+  name                              = "OctoPetShop Web - Staging"
+  resource_group_name               = "octopetshop-demo"
+  roles                             = ["web"]
+  tenanted_deployment_participation = "Untenanted"
+  web_app_name                      = "ops-web-staging"
+}
+
+resource "octopusdeploy_azure_web_app_deployment_target" "ops-web-production" {
+  account_id                        = octopusdeploy_azure_service_principal.octopusdeploy_onmicrosoft_com.id
+  environments                      = [octopusdeploy_environment.production.id]
+  name                              = "OctoPetShop Web - Production"
+  resource_group_name               = "octopetshop-demo"
+  roles                             = ["web"]
+  tenanted_deployment_participation = "Untenanted"
+  web_app_name                      = "ops-web-production"
+}
+
+resource "octopusdeploy_azure_web_app_deployment_target" "ops-api-development" {
+  account_id                        = octopusdeploy_azure_service_principal.octopusdeploy_onmicrosoft_com.id
+  environments                      = [octopusdeploy_environment.development.id]
+  name                              = "OctoPetShop API - Development"
+  resource_group_name               = "octopetshop-demo"
+  roles                             = ["api"]
+  tenanted_deployment_participation = "Untenanted"
+  web_app_name                      = "ops-api-development"
+}
+
+resource "octopusdeploy_azure_web_app_deployment_target" "ops-api-staging" {
+  account_id                        = octopusdeploy_azure_service_principal.octopusdeploy_onmicrosoft_com.id
+  environments                      = [octopusdeploy_environment.staging.id]
+  name                              = "OctoPetShop API - Staging"
+  resource_group_name               = "octopetshop-demo"
+  roles                             = ["api"]
+  tenanted_deployment_participation = "Untenanted"
+  web_app_name                      = "ops-api-staging"
+}
+
+resource "octopusdeploy_azure_web_app_deployment_target" "ops-api-production" {
+  account_id                        = octopusdeploy_azure_service_principal.octopusdeploy_onmicrosoft_com.id
+  environments                      = [octopusdeploy_environment.production.id]
+  name                              = "OctoPetShop API - Production"
+  resource_group_name               = "octopetshop-demo"
+  roles                             = ["api"]
+  tenanted_deployment_participation = "Untenanted"
+  web_app_name                      = "ops-api-production"
+}
